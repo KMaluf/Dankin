af3f5827511d9cbb37df6f279c6fd1daba0b8c35
KMaluf <kailanm1991@gmail.com>
2022-02-20 20:37:35 -0400

    20/02

---
+++ Modulo6/aula7/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    $nome = 'Guilherme';
+
+    switch($nome){
+        case 'Guilherme':
+            echo 'Minha variavel é Guilherme';
+            break;
+        case 'João':
+            echo 'Minha Variavel é joão';
+            break;
+    }
+*/
+
+        //break para loop FOR, WHILE, DO, FOREACH e SWITCH;
+/*
+    for ($i = 0; $i < 10; $i++){
+        echo $i;
+        echo '<hr>';
+        if($i == 5)
+            break;
+    }
+*/
+?>
