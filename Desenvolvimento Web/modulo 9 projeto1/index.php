5251915520e6a66fe9d9f0195e4338ee667e6b28
KMaluf <kailanm1991@gmail.com>
2022-02-23 11:47:50 -0400

    23.02 kosmo

---
+++ Desenvolvimento Web/modulo 9 projeto1/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projeto01</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Descrição do meu website">
+        <meta name="keywords" content="palavra-chave,do,meu,site">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        
+        <header>
+            <div class="logo">Logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <section class="banner-principal">
+            <form >
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name='email'require/>
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </section><!--banner-principal-->
+        <section class="descricao-autor">
+            <div class="w50">
+            <h2>Guilherme C. Grilo</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed risus tellus, pretium quis nibh ac, vulputate molestie nibh. Etiam non erat cursus, pretium odio id, euismod nibh. Pellentesque volutpat, ex ac hendrerit placerat, felis velit elementum nibh, quis sollicitudin justo nibh non neque. Donec a quam nec odio maximus vestibulum vel ac massa. Maecenas in fermentum nunc. Pellentesque vitae aliquet lacus. Nullam vitae commodo lacus, eget rutrum enim. Morbi fringilla facilisis dapibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris maximus sodales condimentum. Pellentesque id ligula sed nisl posuere eleifend. Aenean ut enim id urna sollicitudin elementum. Proin et egestas urna, ultricies finibus ex. Cras blandit vestibulum justo eget feugiat. Donec lacus dolor, molestie vel erat a, sollicitudin convallis elit. Quisque a fermentum diam, nec congue odio.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed risus tellus, pretium quis nibh ac, vulputate molestie nibh. Etiam non erat cursus, pretium odio id, euismod nibh. Pellentesque volutpat, ex ac hendrerit placerat, felis velit elementum nibh, quis sollicitudin justo nibh non neque. Donec a quam nec odio maximus vestibulum vel ac massa. Maecenas in fermentum nunc. Pellentesque vitae aliquet lacus. Nullam vitae commodo lacus, eget rutrum enim. Morbi fringilla facilisis dapibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris maximus sodales condimentum. Pellentesque id ligula sed nisl posuere eleifend. Aenean ut enim id urna sollicitudin elementum. Proin et egestas urna, ultricies finibus ex. Cras blandit vestibulum justo eget feugiat. Donec lacus dolor, molestie vel erat a, sollicitudin convallis elit. Quisque a fermentum diam, nec congue odio.</p>
+            </div><!--w50--> 
+                <img src="https://conteudo.imguol.com.br/c/entretenimento/47/2020/09/15/rowan-atkinson-como-mr-bean-1600189012561_v2_600x800.jpg.webp" width="250">   
+        </section><!--Descricao-autor--->
+
+    </body>
+</html>
