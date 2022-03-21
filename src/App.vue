64a99a955f058e46b2a81496a67b595258e30fb6
KMaluf <92488264+KMaluf@users.noreply.github.com>
2022-03-21 08:53:11 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <TestCliente/>
+
+  </div>
+</template>
+
+<script>
+
+import TestCliente from './components/TestCliente.vue'
+//import ProdutoUm from './components/ProdutoUm.vue'
+export default {
+  name: 'App',
+  components: {
+    TestCliente,
+   // ProdutoUm
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TestCliente.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="cliente">
+        <h2>Cliente, {{ nome }} aqui! </h2>
+        <p>{{ descricao }}</p>
+        
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            nome: "Victor Lima",
+            numero: "",
+            email:"",
+            idade: 0,
+            descricao:" Lorem ipsum Lorem ipsum"
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+    #cliente{
+        max-width: 600px;
+        height: 120px;
+    }
+
+
+</style>
